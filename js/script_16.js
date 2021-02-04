bb176223e9f2b0de3c0e631ee5d75f7c87c656c2
Vladislav <vlad.stepanenko996@gmail.com>
2021-02-05 01:31:43 +1000

    script_16

---
+++ js/script_16.js
@@ -0,0 +1,40 @@
+'use strict';
+
+let num = 20;
+
+function showFirstMessage(text) {
+	console.log(text);
+	// let num = 10;
+	console.log(num);
+}
+
+showFirstMessage('Hello World!');
+console.log(num);
+
+// function calc(a, b) {
+// 	return a + b;
+// }
+
+// console.log(calc(4, 3));
+// console.log(calc(5, 6));
+// console.log(calc(10, 6));
+
+function ret() {
+	let num = 50;
+
+	//
+
+	return num;
+}
+
+const anotherNum = ret();
+console.log(anotherNum);
+
+const logger = function () {
+	console.log('Hello');
+};
+
+logger();
+
+// const calc = (a, b) => a + b;
+const calc = (a, b) => { return a + b; };
