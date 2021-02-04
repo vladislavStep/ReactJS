11563bbe6e629ac0d47f7260311ad5e75c27a663
Vladislav <vlad.stepanenko996@gmail.com>
2021-02-04 17:24:43 +1000

    first commit

--- js/script1.js
+++ js/script1.js
@@ -1,1 +1,24 @@
-console.log('Hello World!');
+// console.log('Hello World!');
+"use strict";
+
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 12;
+console.log(number);
+
+const obj = {
+	a: 50
+};
+
+obj.a = 10;
+console.log(obj.a);
+
+console.log(name);
+var name = 10;
+
+{
+	var result = 50;
+}
+
+console.log(result);
