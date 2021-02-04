7aa97a3bcc1fe0bde1ac9170b98c4ac4edcf2f06
Vladislav <vlad.stepanenko996@gmail.com>
2021-02-05 01:42:51 +1000

    script_17

---
+++ js/script_17.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const str = 'test';
+const arr = [1, 2, 4];
+
+// console.log(str[d] = 'd');
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str);
+
+const fruit = 'Some fruit';
+console.log(fruit.indexOf('fruit'));
+
+const logg = 'Hello world';
+
+console.log(logg.slice(6));
+console.log(logg.substring(6, 11));
+
+console.log(logg.substr(6, 4));
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = '12.2px';
+console.log(parseInt(test));
+console.log(parseFloat(test));
