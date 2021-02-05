ca51623f1963f722d4ea46764cd86d9638ebad64
Vladislav <vlad.stepanenko996@gmail.com>
2021-02-05 17:59:46 +1000

    script_19.js

---
+++ js/script_19.js
@@ -0,0 +1,28 @@
+'use strict';
+
+function first() {
+	// Do something
+	setTimeout(function () {
+		console.log(1);
+	}, 500);
+}
+
+function second() {
+	console.log(2);
+}
+
+first();
+second();
+
+function learnJS(lang, callBack) {
+	console.log(`Я учу: ${lang}`);
+	callBack();
+}
+function done() {
+	console.log('Я прошол этот урок ');
+}
+// learnJS('JavaScript', function () {
+// 	console.log('Я прошол этот урок ');
+// });
+
+learnJS('JavaScript', done)
