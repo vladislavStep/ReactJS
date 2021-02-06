7920484fc5b6f547081c9ac6661b4712b8c75803
Vladislav <vlad.stepanenko996@gmail.com>
2021-02-07 00:51:57 +1000

    script_23

---
+++ js/script_23.js
@@ -0,0 +1,28 @@
+'use strict';
+
+let str = 'some';
+let strObj = new String(str);
+
+console.log(typeof (str));
+console.log(typeof (strObj));
+
+console.dir([1, 2, 3]);
+
+const soldier = {
+	health: 400,
+	armor: 100,
+	sayHello: function () {
+		console.log('Hello');
+	}
+};
+const jonh = Object.create(soldier);
+// const jonh = {
+// 	health: 100
+// };
+
+// jonh.__proto__ = soldier;
+
+// Object.setPrototypeOf(jonh, soldier);
+
+// console.log(jonh.armor);
+jonh.sayHello();
