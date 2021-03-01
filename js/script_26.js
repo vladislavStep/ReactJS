53a303201ba4b6d3694ebfaf60d9867a8c6e091b
Vladislav <vlad.stepanenko996@gmail.com>
2021-03-01 22:38:18 +1000

    script_2

---
+++ js/script_26.js
@@ -0,0 +1,44 @@
+'use strict';
+
+// To String
+
+// 1
+console.log(typeof (String(null)));
+console.log(typeof (String(4)));
+
+// 2
+console.log(typeof (5 + ''));
+const num = 5;
+console.log("https://vk.com.catalog/" + num);
+const fontSize = 20 + 'px';
+
+// To Number
+
+// 1
+console.log(typeof (Number('4')));
+
+// 2
+console.log(typeof (+'4'));
+
+// 3
+console.log(typeof (parseInt('15px', 10)));
+
+let answ = +prompt('Hello', '');
+
+// To Boolean
+// 0, '', null, undefined, NaN;
+// 1
+let switcher = null;
+if (switcher) {
+	console.log('Working...');
+}
+switcher = 1;
+if (switcher) {
+	console.log('Working...');
+}
+
+// 2
+console.log(typeof (Boolean('4')));
+
+// 3
+console.log(typeof (!!'4'));
