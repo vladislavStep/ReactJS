5239b9f823825b4aabbb39596ab2fbcba3b7434a
Vladislav <vlad.stepanenko996@gmail.com>
2021-05-25 15:37:47 +1000

    stript_28_2.js

---
+++ js/script_28.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// let timestamp = Date.now();
+// let now = new Date();
+// let ms = now.getTime();
+
+// let iso = now.toISOString();
+// console.log(timestamp);
+// console.log(now);
+// console.log(ms);
+// console.log(iso);
+// console.log(NaN === undefined);
+
+// console.log(3);
+/*
+
+*/
